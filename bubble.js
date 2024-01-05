f6fbd3b740605fd5b7f7be7196f8faf05332507a
jasonscotch <jason.m.scot@gmail.com>
2024-01-04 20:38:44 -0800

    initial upload of Sorting Exercise homework

---
+++ bubble.js
@@ -0,0 +1,16 @@
+function bubbleSort(arr) {
+    let count = 0;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            count++;
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+module.exports = bubbleSort;
---
+++ quick.js
@@ -0,0 +1,39 @@
+/*
+pivot accepts an array, starting index, and ending index
+You can assume the pivot is always the first element
+*/
+
+function pivot(arr, start = 0, end = arr.length - 1) {
+    const swap = (array, i, j) => {
+        [array[i], array[j]] = [array[j], array[i]];
+    };
+
+    let pivotValue = arr[start];
+    let pivotIndex = start;
+
+    for (let i = start + 1; i <= end; i++) {
+        if (arr[i] < pivotValue) {
+            pivotIndex++;
+            swap(arr, i, pivotIndex);
+        }
+    }
+    swap(arr, start, pivotIndex);
+
+    return pivotIndex;
+}
+
+/*
+quickSort accepts an array, left index, and right index
+*/
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+    if (left < right) {
+        let pivotIndex = pivot(arr, left, right);
+
+        quickSort(arr, left, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, right);
+    }
+    return arr;
+}
+
+module.exports = {pivot, quickSort};
